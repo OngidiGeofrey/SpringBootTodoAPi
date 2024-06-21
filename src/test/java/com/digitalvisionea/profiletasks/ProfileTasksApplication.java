13f767d97b9176128018938dee9127cb2ae95079
ONGIDI E. GEOFREY <ongidigeofrey@gmail.com>
2024-06-21 12:19:29 +0300

    Fix: a todo list connection

---
+++ src/test/java/com/digitalvisionea/profiletasks/ProfileTasksApplication.java
@@ -0,0 +1,13 @@
+package com.digitalvisionea.profiletasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class ProfileTasksApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileTasksApplication.class, args);
+	}
+}
